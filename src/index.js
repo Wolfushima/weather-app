52b3288559d486dd9667aafaf8f5279d52194529
Adolfo Herrera <valherdolf@gmail.com>
2022-05-23 15:24:22 -0400

    Basic layout of the webpage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import run from './app/app';
-import './main.scss';
+import './scss/style.scss';
 
+async function getWeather() {
+    try {
+        const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=a24c2c29779f769bcd957c131159a94d', { mode: 'cors' });
+        console.log(response);
+        const weatherData = await response.json();
+        console.log(weatherData);
+    } catch (err) {
+        console.log(err);
+    }
+}
+getWeather();
 run();
--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,11 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
     mode: 'development',
+    devServer: { // make webpack reload the web browser every save
+        hot: false, // optional, but you must not set both hot and liveReload to true
+        liveReload: true,
+    },
+    devtool: 'source-map',
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
